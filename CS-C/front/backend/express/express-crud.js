2e206b06c7fa4869f8a18bfd27cd125bcf343b09
Shivam Patel <shivampatel1743@gmail.com>
2025-02-27 10:22:17 +0530

    updated on 27/02/2025

---
+++ CS-C/front/backend/express/express-crud.js
@@ -0,0 +1,43 @@
+import express from 'express';
+const app =express();
+const  users =[{
+    id:1,
+    name: 'abhishek',
+    age: 30
+}]
+const port=3000;
+app.use(express.json());
+
+app.get('/', (req,res)=>{
+    res.send("hello cs c");
+});
+
+app.get('/users/:id', (req,res)=>{
+   const {id} = req.params;
+   const user = users.find(user => user.id == id);
+   res.send(user);
+});
+
+app.post('/users', (req,res)=>{
+    const {name,age} = req.body;
+    const newID = users.length>0 ? users[users.length-1].id+1:1;
+
+    users.push({id: newID,name,age});
+    res.send("new user is created successfully");
+});
+
+app.get('/users', (req,res)=>{
+    res.send(users)
+})
+//middle ware express.text()
+//method app.use
+
+app.post('/', (req,res)=>{
+    const data = req.body;
+    console.log(data);
+    res.send("data added successfully");
+});
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`);
+
+});
